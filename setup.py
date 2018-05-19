2e6fbf550bb5fe3a39d63e07292aa5be0f74d5d1
Max Zheng <maxzheng.os@gmail.com>
2018-05-19 20:25:46 +0000

    Publish version 0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='ansible-hostmanager',
-    version='0.2.2',
+    version='0.2.3',
 
     author='Max Zheng',
     author_email='maxzheng.os@gmail.com',
